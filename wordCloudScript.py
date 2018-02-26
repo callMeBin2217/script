ed02006c4beb5642be23bf8eb88507f2ce68e336
HuBin <343603146@qq.com>
2018-02-26 09:26:26 +0800

    update

--- wordCloudScript.py
+++ wordCloudScript.py
@@ -14,10 +14,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import time
 from pandas import Series,DataFrame
+import getpass
 
 
 def makeWordCloud():
-	with open(r'C:/Users/SleepAutumnD/Desktop/fright.txt','r',encoding='utf-8') as f:
+	sys_name = getpass.getuser()
+	target_file = 'wyComment.txt'
+	with open(r'C:/Users/'+sys_name+'/Desktop/'+target_file,'r',encoding='utf-8') as f:
 		data = f.read()
 		pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]')
 		data = re.findall(pattern,data)
@@ -26,15 +29,15 @@ def makeWordCloud():
 	segment = jieba.lcut(str(data))
 	words_df = DataFrame({'segment':segment})
 	#去掉停用词,quoting=3全不引用
-	stopwords = pd.read_csv(r'C:/Users/SleepAutumnD/Desktop/stopwords.txt',index_col=False,quoting=3,sep='\t',names=['stopword'],encoding='utf-8')
+	stopwords = pd.read_csv(r'C:/Users/'+sys_name+'/Desktop/stopwords.txt',index_col=False,quoting=3,sep='\t',names=['stopword'],encoding='utf-8')
 	words_df = words_df[~words_df.segment.isin(stopwords.stopword)]
 	#统计词频
 	words_stat = words_df.groupby(by=['segment'])['segment'].agg({'计数':np.size})
 	words_stat = words_stat.reset_index().sort_values(by=['计数'],ascending=False)
 	#用词云进行显示
-	back_img = plt.imread(r'C:/Users/SleepAutumnD/Desktop/color.jpg')
+	back_img = plt.imread(r'C:/Users/'+sys_name+'/Desktop/color.jpg')
 	img_color = ImageColorGenerator(back_img)
-	wordcloud = WordCloud(mask=back_img,font_path='simhei.ttf',background_color='white',max_font_size=100,min_font_size=20,random_state=42)
+	wordcloud = WordCloud(mask=back_img,font_path='simhei.ttf',background_color='white',max_font_size=200,min_font_size=20,random_state=42,max_words=300)
 	word_frequence = {x[0]:x[1] for x in words_stat.head(1000).values}
 
 
