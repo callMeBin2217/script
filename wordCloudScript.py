06f78a911179983af4fe10ef551e6b98ea5a19cd
HuBin <343603146@qq.com>
2018-03-02 10:33:46 +0800

    .

--- wordCloudScript.py
+++ wordCloudScript.py
@@ -15,19 +15,23 @@ import pandas as pd
 import time
 from pandas import Series,DataFrame
 import getpass
+from collections import Counter	
 
 
 def makeWordCloud():
 	sys_name = getpass.getuser()
-	target_file = 'wyComment.txt'
+	target_file = 'wyComment2.txt'
 	with open(r'C:/Users/'+sys_name+'/Desktop/'+target_file,'r',encoding='utf-8') as f:
 		data = f.read()
-		pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9]')
+		pattern = re.compile(r'[\u4e00-\u9fa5a-zA-Z0-9].*')
 		data = re.findall(pattern,data)
 	#print(data)
 	#使用结巴分词进行中文分词
+	#jieba.add_word('白色球鞋')
 	segment = jieba.lcut(str(data))
+	print(Counter(segment)['吴佳怡'])
 	words_df = DataFrame({'segment':segment})
+	print(words_df)
 	#去掉停用词,quoting=3全不引用
 	stopwords = pd.read_csv(r'C:/Users/'+sys_name+'/Desktop/stopwords.txt',index_col=False,quoting=3,sep='\t',names=['stopword'],encoding='utf-8')
 	words_df = words_df[~words_df.segment.isin(stopwords.stopword)]
@@ -37,7 +41,7 @@ def makeWordCloud():
 	#用词云进行显示
 	back_img = plt.imread(r'C:/Users/'+sys_name+'/Desktop/color.jpg')
 	img_color = ImageColorGenerator(back_img)
-	wordcloud = WordCloud(mask=back_img,font_path='simhei.ttf',background_color='white',max_font_size=200,min_font_size=20,random_state=42,max_words=300)
+	wordcloud = WordCloud(mask=back_img,font_path='simhei.ttf',background_color='white',max_font_size=200,min_font_size=20,random_state=42,max_words=500)
 	word_frequence = {x[0]:x[1] for x in words_stat.head(1000).values}
 
 
